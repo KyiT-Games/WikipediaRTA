91e43649eb2ca95b58a5dddc7e7d216daea66fd7
Kihamda <kuawse580@gmail.com>
2024-02-27 00:08:16 +0900

    Wikipediaの中身の表示以外について、それっぽい動作をするようにした

--- src/js/script.js
+++ src/js/script.js
@@ -11,13 +11,46 @@ const wikiFetch = async () => { //asyncで非同期処理だと宣言する
 
     const valueJson = await fetchValue; //非同期処理を実行
     const articles = valueJson.query.pageids; //取得したデータを配列に格��
-    console.log(articles) //必要な情報が入っている配列を取得
+    return articles; //必要な情報が入っている配列を取得
 };
+
+const wikiLoad = async (articleid) => { //asyncで非同期処理だと宣言する
+  const fetchValue = fetch(`https://ja.wikipedia.org/w/api.php?action=parse&pageid=${articleid}&format=json&origin=*`, {
+    method: "GET"
+  })
+    .then((value) => { //データが取得できればこっち
+      return value.json(); //wikipediaからのデータをJSON形式に変換
+    })
+    .catch(() => { //取得に失敗すればこっち
+      alert("wikipediaにうまくアクセスできないようです、、");
+    });
+
+  const valueJson = await fetchValue; //非同期処理を実行
+  const articles = valueJson.parse.text; //取得したデータを配列に格��
+  return articles; //必要な情報が入っている配列を取得
+};
+
+function displayOnOff(onoff) { //hiddenframeの表示/非表示を切り替える
+  if (onoff == true){
+    $(".hiddenframe").css("display", "none");
+  }else{
+    $(".hiddenframe").css("display", "block");
+  }
+}
 
 document.getElementById("myBtn").addEventListener("click", function() {
     $(".titleframe").css("display", "none");
+    
+    wikiFetch().then(article => { //then節の中に2つ読み込まれてから実行される
+      console.log(article);
+      wikiLoad(article[0]).then(articlehtml => {  //記事が読み込まれてから実行される。
+        console.log(articlehtml);
+        var iframe = document.getElementById('target');
+        $(".wikiframe").css("display", "block");
+        $(".gameframe").css("display", "block");
 
-    wikiFetch();
-    $(".wikiframe").css("display", "block");
-    $(".gameframe").css("display", "block");
+      });
+
+    });
+
 });
